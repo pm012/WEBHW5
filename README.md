7b473ba3d846dd53fe67a4bdaab0d71d736051e5
Serhii Kroshka <pm012@ukr.net>
2024-03-30 22:17:53 +0200

    First part of HW1 and radded description to readme file

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
 # WEBHW5
+
 Home work for 5 module
+Create console app that will return exchange rate of EUR and USD for serveral last days from PrivatBank arhive using it's API
+(for example, https://api.privatbank.ua/p24api/exchange_rates?json&date=01.12.2022).
+Set limit to let user know currency exchange rate not more than for 10 last days.
+For requests to API use Aiohttp client.
+Adhere SOLID principles during implementation of the task.
+Handle network errors properly
+
+Example call:
+py .\main.py 2
+
+Result:
+[
+{
+'03.11.2022': {
+'EUR': {
+'sale': 39.4,
+'purchase': 38.4
+},
+'USD': {
+'sale': 39.9,
+'purchase': 39.4
+}
+}
+},
+{
+'02.11.2022': {
+'EUR': {
+'sale': 39.4,
+'purchase': 38.4
+},
+'USD': {
+'sale': 39.9,
+'purchase': 39.4
+}
+}
+}
+]
+
+Additional Part
+
+- Add possibility to pass additional currencies via parameters
+- Add to the chat on sockets ability to write command exchange that will show users current exchange rate in a text format
+- Extend added command exchange, to have the ability to get exchange rate in a chat for the last several days. For example, exchange 2
+- with the help of packages auifile and aiopath add logging to the file, when exchange was called in a chat
---
+++ main.py
@@ -0,0 +1,64 @@
+import asyncio
+import aiohttp
+import pprint
+
+class ExchangeRateFetcher:
+    def __init__(self):
+        self.base_url = 'https://api.privatbank.ua/p24api/exchange_rates'
+
+    async def fetch_exchange_rate(self, session, date):
+        url = f"{self.base_url}?date={date}"
+        async with session.get(url) as response:
+            if response.status == 200:
+                return await response.json()
+            else:
+                return None
+
+    async def get_exchange_rates(self, dates):
+        async with aiohttp.ClientSession() as session:
+            tasks = [self.fetch_exchange_rate(session, date) for date in dates]
+            results = await asyncio.gather(*tasks, return_exceptions=True)
+            return results
+
+def format_exchange_rate_data(data):
+    formatted_data = []
+    for entry in data:
+        if entry is not None:
+            rates = entry['exchangeRate']
+            formatted_entry = {entry['date']: {'EUR': {}, 'USD': {}}}
+            for rate in rates:
+                if rate['currency'] == 'EUR':
+                    formatted_entry[entry['date']]['EUR']['sale'] = rate.get('saleRate', rate['saleRateNB'])
+                    formatted_entry[entry['date']]['EUR']['purchase'] = rate.get('purchaseRate', rate['purchaseRateNB'])
+                elif rate['currency'] == 'USD':
+                    formatted_entry[entry['date']]['USD']['sale'] = rate.get('saleRate', rate['saleRateNB'])
+                    formatted_entry[entry['date']]['USD']['purchase'] = rate.get('purchaseRate', rate['purchaseRateNB'])
+            formatted_data.append(formatted_entry)
+    return formatted_data
+
+async def main():
+    import sys
+    if len(sys.argv) != 2:
+        print("Usage: python main.py <number_of_days>")
+        return
+
+    try:
+        num_days = int(sys.argv[1])
+        if num_days > 10:
+            print("Number of days should not exceed 10.")
+            return
+    except ValueError:
+        print("Invalid input. Please provide a valid number of days.")
+        return
+
+    dates = [(datetime.date.today() - datetime.timedelta(days=i)).strftime("%d.%m.%Y") for i in range(num_days)]
+    exchange_rate_fetcher = ExchangeRateFetcher()
+    exchange_rate_data = await exchange_rate_fetcher.get_exchange_rates(dates)
+    formatted_data = format_exchange_rate_data(exchange_rate_data)
+    #print(formatted_data)
+    pp = pprint.PrettyPrinter(indent=4)
+    pp.pprint(formatted_data)
+
+if __name__ == "__main__":
+    import datetime
+    asyncio.run(main())
